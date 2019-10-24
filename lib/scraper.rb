bfe6812ca453a798c28a8d4987fc1b075e4361ac
Huda Yousif <hjdoski@gmail.com>
2019-10-24 00:20:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,16 +12,7 @@ class Scraper
     
   end 
   
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
+
   
 end
 
