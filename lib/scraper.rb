83682dd1c316213ea4e5fd794fac461d2434720b
Huda Yousif <hjdoski@gmail.com>
2019-10-24 00:16:46 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,6 +7,9 @@ class Scraper
   
   def get_page 
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    
+    binding.pry 
+    
   end 
   
   def print_courses
