fb33c17a24d5e32876682b6dce0bc9ed90b6d56e
Huda Yousif <hjdoski@gmail.com>
2019-10-24 00:37:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,7 +21,7 @@ class Scraper
   self.get_page.css(".post")
   end
 
-    def make_courses
+  def make_courses
     self.get_courses.each do |post|
       course = Course.new
       course.title = post.css("h2").text
